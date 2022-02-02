16e14230ed75f69e28cc1c333fa9f8f85bd501c8
Shawn Li <syl52@cam.ac.uk>
2022-02-02 00:30:43 +0000

    1E and modified geo file. problem with stations object not being iterable?

---
+++ Task1E.py
@@ -0,0 +1,13 @@
+'''TASK 1E'''
+
+from distutils.command.build import *
+from tkinter import N
+from floodsystem.geo import *
+from floodsystem.station import *
+from floodsystem.datafetcher import *
+from floodsystem.stationdata import build_station_list
+
+# Run the demonstration with N = 9
+stations = build_station_list
+rivers_by_station_number(stations, 9)
+
