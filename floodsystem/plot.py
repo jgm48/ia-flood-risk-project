f51396edbd703aa1f0ec6cd0a5a107e2e1a754bf
Shawn Li <syl52@cam.ac.uk>
2022-03-01 17:11:55 +0000

    yassification of task 2e

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -8,15 +8,15 @@ def plot_water_levels(station, dates, levels):
 
     # plot the low and high typical values
     low_typ_range_list = len(dates) * [station.typical_range[0]]
-    plt.plot(dates, low_typ_range_list, 'r')
+    plt.plot(dates, low_typ_range_list, 'r', label='low level')
     high_typ_range_list = len(dates) * [station.typical_range[1]]
-    plt.plot(dates, high_typ_range_list, 'r')
+    plt.plot(dates, high_typ_range_list, 'g', label='high level')
 
     # plot the water level data
     if dates == [] or levels == []:
         return "Invalid data"
     else:
-        plt.plot(dates, levels, 'g')
+        plt.plot(dates, levels, 'b', label='water level')
     
     # setting up graph layout
     plt.xlabel('Date')
@@ -24,6 +24,11 @@ def plot_water_levels(station, dates, levels):
     plt.xticks(rotation=45)
     plt.title(station.name)
     plt.tight_layout()
+    plt.legend(loc='best')
+
+    # maximise the window
+    manager = plt.get_current_fig_manager()
+    manager.full_screen_toggle()
     
     # gimmie dat plot!
     plt.show()
