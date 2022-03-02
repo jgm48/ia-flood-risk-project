27fea3645225892292cfe9d893ddd1bd8ac5ef0c
Shawn Li <syl52@cam.ac.uk>
2022-03-02 02:42:52 +0000

    updated test_analysis for 2g

--- test_analysis.py
+++ test_analysis.py
@@ -7,7 +7,7 @@ import numpy as np
 from floodsystem.datafetcher import fetch_measure_levels
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.analysis import polyfit
-#from floodsystem.analysis import risk_level_towns
+from floodsystem.analysis import risk_level_towns
 
 def test_polyfit():
     """Tests polyfit function"""
@@ -25,5 +25,17 @@ def test_polyfit():
         for n in range(2, 4):
             assert type(polyfit(dates, levels, n)[1]) == np.float64
             assert type(polyfit(dates, levels, n)[0]) == np.poly1d
+
+def test_risk_level_towns():
+    """Tests risk_level_towns function"""
+    stations = build_station_list()
+    update_water_levels(stations)
+
+    severe, high, moderate, low = risk_level_towns()
+
+    # check that most towns belong to a risk level
+    for i in range(0, len(stations) - 1, 250):
+        assert len(stations[i].town in severe or stations[i].town in high or stations[i].town in moderate or stations[i].town in low) > 0.7*len(stations)
+
             
 
