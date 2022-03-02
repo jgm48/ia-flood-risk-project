7e6987859c3d249fcf7bcb30042060a6881d5f65
Shawn Li <syl52@cam.ac.uk>
2022-03-02 10:12:19 +0000

    fixed one unit test, polyfit still not wokring

--- test_analysis.py
+++ test_analysis.py
@@ -5,8 +5,7 @@
 import datetime
 import numpy as np
 from floodsystem.stationdata import build_station_list, update_water_levels
-from floodsystem.analysis import polyfit
-from floodsystem.analysis import risk_level_towns
+from floodsystem.analysis import polyfit, risk_level_towns
 from floodsystem.datafetcher import fetch_measure_levels
 
 def test_polyfit():
@@ -20,11 +19,11 @@ def test_polyfit():
     # output data types
     for x in range(1, 1500, 100):
         i = stations[x]
-        dates, levels = fetch_measure_levels(i.measure_id,
-        dt=datetime.timedelta(days=dt))
+        dates, levels = fetch_measure_levels(i.measure_id, dt=datetime.timedelta(days=dt))
         for n in range(2, 4):
-            assert type(polyfit(dates, levels, n)[1]) == np.float64
             assert type(polyfit(dates, levels, n)[0]) == np.poly1d
+            assert type(polyfit(dates, levels, n)[1]) == np.float64
+            
 
 def test_risk_level_towns():
     """Tests risk_level_towns function"""
@@ -35,7 +34,8 @@ def test_risk_level_towns():
 
     # check that most towns belong to a risk level
     for i in range(0, len(stations) - 1, 250):
-        assert len(stations[i].town in severe or stations[i].town in high or stations[i].town in moderate or stations[i].town in low) > 0.7*len(stations)
+        assert stations[i].town in severe or stations[i].town in high or stations[i].town in moderate or stations[i].town in low
+
 
             
 
